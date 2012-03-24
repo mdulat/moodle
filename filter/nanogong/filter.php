47e8d6bf2f6b4e496ebf2fb76efbada12ed60cb5
Margaret Dulat <mdulat@gmail.com>
2012-03-23 17:42:58 -0700

    filter works but positioning a little wierd still

--- filter/nanogong/filter.php
+++ filter/nanogong/filter.php
@@ -20,48 +20,36 @@ static $loaded = false;
     var loaded_nanogong_id = -1;
     
     function set_nanogong_visible(visible) {
-        var div = document.getElementById("nanogong_applet_container_for_embed");
-        if (div) div.style.visibility = (visible)? "visible" : "hidden";
-    }
-    
-    function set_nanogong_position(obj) {
-        //var div = document.getElementById("nanogong_applet_container_for_embed");
-        //var span = document.getElementById("nanogong_span_"+current_nanogong_id);
-        //var position = $(object).position();
-        //var curTop = span.offsetTop;
-        //var curLeft = span.offsetleft;
-        //alert("cur top = " + curTop);
-        //alert("cur left = " + curLeft);
-        
-        //div.style.top = curTop + "px";
-        //div.style.left = curLeft + "px";
+        var container = document.getElementById("nanogong_applet_container_for_embed");
+        if (visible)
+            container.style.visibility = "visible";
+        else
+            container.style.visibility = "hidden";
     }
     
     function show_nanogong_applet(obj, id, url) {
       if (current_nanogong_id != id)
       {
+        $("#nanogong_applet_container_for_embed").insertAfter("#nanogong_span_"+id);
         current_nanogong_id = id;
-        $("#nanogong_applet_container_for_embed").insertAfter("#nanogong_span_"+current_nanogong_id);
-        //set_nanogong_position(obj);
-        set_nanogong_visible(true);
-    
-        if(loaded_nanogong_id != id) {
+      }
+      
+      set_nanogong_visible(true);
+      
+      if(loaded_nanogong_id != id) {
             document.getElementById("embedded_nanogong_player").sendGongRequest("LoadFromURL", url);
             loaded_nanogong_id = id;
         }
-      }
     }
     
-    function hide_nanogong_applet(id) {
-      if (current_nanogong_id == id){
-        set_nanogong_visible(false);
-        current_nanogong_id = -1;
-        }
+    function is_nanogong_applet_visible() {
+        //return document.getElementById("nanogong_applet_container_for_embed").style.visibility == "visible";
+        return $("#nanogong_applet_container_for_embed").css("visibility") == "visible";
     }
     
     function onclick_nanogong_applet(obj, id, url) {
-        if (current_nanogong_id == id)
-            hide_nanogong_applet(id);
+        if (current_nanogong_id == id && is_nanogong_applet_visible())
+            set_nanogong_visible(false);
         else
             show_nanogong_applet(obj, id, url);
     }
